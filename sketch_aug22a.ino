b43da5032a2c1a93bc49ee31f9677b3abf603074
BrawnyClover <sonbill971@gmail.com>
2022-08-22 17:49:43 +0900

    Initial commit

---
+++ sketch_aug22a.ino
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(7, 8);
+
+void forward1() {
+  Serial.write("forward\n");
+  digitalWrite(3, LOW); //�ㅻⅨ履� �꾨옒
+  digitalWrite(9, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(4, LOW); //�쇱そ �꾨옒
+}
+void backward1() {
+  Serial.write("backward\n");
+  digitalWrite(3, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(4, HIGH);
+}
+void turnleft() {
+  Serial.write("left\n");
+  digitalWrite(3, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(4, LOW);
+}
+void turnright() {
+  Serial.write("right\n");
+  digitalWrite(3, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(4, HIGH);
+}
+
+void stopall() {
+  Serial.write("stop\n");
+  digitalWrite(3, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(4, LOW);
+}
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(4, OUTPUT);
+  BT.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.flush();
+  // �⑥닔瑜� �ъ슜�섏뿬 �꾩쭊,�꾩쭊,醫뚰쉶��,�고쉶��,�뺤�
+  if (BT.available()) {
+    char c = BT.read();
+//    Serial.write("%c\n",c);
+    if (c == 'f')forward1();
+    if (c == 'b')backward1();
+    if (c == 'l')turnleft();
+    if (c == 'r')turnright();
+    if (c == 's')stopall();
+  }
+}
